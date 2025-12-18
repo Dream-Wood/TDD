e8b4a39e2a9d009c4c5fe2dc9cfb81bce7feeea2
Dream Wood <VladGggolubev2015@gmail.com>
2025-12-18 13:45:38 +0300

    Initial commit

---
+++ TDD/CustomList.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace TDD;
+
+public class CustomList<T>: IEnumerable<T>
+{
+    public int Count { get; private set; }
+    public int Capacity { get; set; }
+    private const int DefaultCapacity = 4;
+    public CustomList():this(DefaultCapacity){}
+
+    private T[] _items;
+
+    public CustomList(int size)
+    {
+        Capacity = size;
+        Count = 0;
+    }
+
+    public CustomList(IEnumerable<int> collection)
+    {
+        ArgumentNullException.ThrowIfNull(collection);
+
+        Capacity = collection.Count();
+        _items = new T[Capacity];
+        Array.Copy(collection.ToArray(), 0, _items, 0, Capacity);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            yield return _items[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            yield return _items[i];
+        }
+    }
+}
---
+++ TDDTestProject/UnitTest1.cs
@@ -0,0 +1,55 @@
+using TDD;
+
+namespace TDDTestProject;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void CountMustBeZeroAfterListCreation()
+    {
+        CustomList<int> list = new CustomList<int>();
+        Assert.That(list.Count, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void CapacityCanSetByListCreation()
+    {
+        int expectedCapacity = 8;
+        CustomList<int> list = new CustomList<int>(expectedCapacity);
+        Assert.That(list.Capacity, Is.EqualTo(expectedCapacity));
+    }
+    
+    [Test]
+    public void ExceptionIfConstructorTakeNullCollection()
+    {
+        IEnumerable<int> collection = null;
+        Assert.Throws<ArgumentNullException>(() => new CustomList<int>(collection));
+    }
+
+    [Test]
+    public void CapacityIsEqualCollectionLenghtAfterListCreation()
+    {
+        IEnumerable<int> collection = [1,2,3,5];
+        var list = new CustomList<int>(collection);
+        Assert.IsTrue(list.Capacity == collection.Count());
+    }
+
+    [Test]
+    public void CollectionItemsExistAfterListCreation()
+    {
+        int[] collection = [1,2,3,5];
+        var list = new CustomList<int>(collection);
+        int i = 0;
+        foreach (var item in list)
+        {
+            Assert.IsTrue(item == collection[i]);
+            i++;
+        }
+        Assert.IsTrue(collection.Length == i);
+    }
+}
